4b200b4f96878733744e2b8534e361ad8de1cdd1
unknown <dahersaer20@gmail.com>
2023-08-27 14:02:32 +0300

    linking my web app to online API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function goahead(){
-    fetch('http://localhost:1234/ahmad')
+    // fetch('http://localhost:1234/ahmad')
+    fetch('https://test20230827.onrender.com/ahmad')
       .then(response => response.json())
       .then(json => {
         let studentul= document.getElementById('student');
